aba72508d068dcd7d96200da1de2cbe438064320
liuwill <liuwei_will@qq.com>
2018-10-09 17:27:07 +0800

    print radix tree for check stucture

--- radix_trees/tree_test.go
+++ radix_trees/tree_test.go
@@ -4,18 +4,49 @@ import (
 	"testing"
 )
 
-func Test_RadixSearch(t *testing.T) {
-	source := []string{"api", "list", "new", "newest", "little"}
-	wordExist := "api"
-	wordNotExist := ""
-
-	tree := buildRadixTree(source)
-	if !searchRadix(tree, wordExist) {
-		t.Error("Test_RadixSearch Exist Fail", wordExist)
+func printTree(tree *RadixTree) {
+	stack := []*TreeNode{
+		tree.root,
 	}
 
-	if searchRadix(tree, wordNotExist) {
-		t.Error("Test_RadixSearch NotExist Fail", wordNotExist)
+	top := 0
+	for top < len(stack) {
+		currentNode := stack[top]
+		top++
+
+		println("node:", currentNode, "=>[", currentNode.start, ":", currentNode.current, "]")
+		for _, node := range currentNode.children {
+			stack = append(stack[:], node)
+
+			println("  Child: ", node, " [Start:", node.start, ", Current:", node.current, "]")
+		}
+
+		println("---------------------------------------------------------")
+	}
+}
+
+func Test_RadixSearch(t *testing.T) {
+	source := []string{"api", "list", "new", "newest", "little", "long"}
+	wordExists := []string{
+		// "api",
+		"long",
+	}
+	wordNotExists := []string{
+	// "", "ap", "lo", "longer", "newer", "ling",
+	}
+
+	tree := buildRadixTree(source)
+	printTree(tree)
+	for _, isExist := range wordExists {
+		if !searchRadix(tree, isExist) {
+			t.Error("Test_RadixSearch Exist Fail", isExist)
+		}
+	}
+
+	for _, notExist := range wordNotExists {
+		if searchRadix(tree, notExist) {
+			t.Error("Test_RadixSearch NotExist Fail", notExist)
+		}
 	}
 
 	t.Log("Test_RadixSearch Radix Search Tree")
