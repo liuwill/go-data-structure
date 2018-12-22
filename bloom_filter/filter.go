ab7f4674dafa68416c8fd7aa9d80f416569addf2
liuwill <liuwei_will@qq.com>
2018-12-22 16:45:50 +0800

    finish simplest bloom filter without hash

--- bloom_filter/filter.go
+++ bloom_filter/filter.go
@@ -1,24 +1,55 @@
 package bloom_filter
 
 type BloomFilter struct {
-	hash []bool
-	size int
+	Hash []bool
+	Size int
 }
 
 func newBloomFilterInstance(size int) *BloomFilter {
-	if size <= 0 {
-		size = 100
-	}
+	// if size <= 0 {
+	// 	size = 100
+	// }
+
 	return &BloomFilter{
-		hash: make([]bool, size),
-		size: size,
+		Hash: make([]bool, size),
+		Size: size,
 	}
 }
 
 func (filter *BloomFilter) insert(input string) {
-
+	inputHash := filter.findHash(input)
+	for _, pos := range inputHash {
+		filter.Hash[pos] = true
+	}
 }
 
 func (filter *BloomFilter) contain(input string) bool {
-	return false
+	inputHash := filter.findHash(input)
+	for _, pos := range inputHash {
+		if filter.Hash[pos] == false {
+			return false
+		}
+	}
+	return true
+}
+
+var hashList = []func(intput string, size int) int{
+	func(intput string, size int) int {
+		return 0
+	},
+	func(intput string, size int) int {
+		return 0
+	},
+	func(intput string, size int) int {
+		return 0
+	},
+}
+
+func (filter *BloomFilter) findHash(input string) []int {
+	hashResults := make([]int, len(hashList))
+	for i, _ := range hashList {
+		hashResults[i] = hashList[i](input, filter.Size)
+	}
+
+	return hashResults
 }
